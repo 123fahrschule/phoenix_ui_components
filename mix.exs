7344d6a9bb7b786b1a6b20ed49ad91e3b68b1396
Illia Melnykov <33714167+illiamelnykov@users.noreply.github.com>
2022-09-16 10:55:18 +0300

    Add dependencies

--- mix.exs
+++ mix.exs
@@ -21,8 +21,10 @@ defmodule PhoenixUiComponents.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # Productive. Reliable. Fast. A productive web framework that does not compromise speed and maintainability.
+      {:phoenix, "~> 1.6"},
+      # Rich, real-time user experiences with server-rendered HTML
+      {:phoenix_live_view, "~> 0.17.11"}
     ]
   end
 end
