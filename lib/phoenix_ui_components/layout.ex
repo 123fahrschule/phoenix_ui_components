f17fac362252602c64f1fd104db035790f3a3a8a
Illia Melnykov <33714167+illiamelnykov@users.noreply.github.com>
2022-09-19 09:56:06 +0300

    Add component for root layout

--- lib/phoenix_ui_components/layout.ex
+++ lib/phoenix_ui_components/layout.ex
@@ -1,6 +1,35 @@
 defmodule PhoenixUiComponents.Layout do
   use PhoenixUiComponents, :component
 
+  # slot :head, default: []
+  # slot :inner_block, required: true
+  def root_layout(assigns) do
+    assigns = assigns |> assign_slot(:head)
+
+    ~H"""
+    <!DOCTYPE html>
+    <html lang="de" class="h-full">
+      <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <%= render_slot(@head) %>
+        <style>
+          [x-cloak] { display: none !important; }
+          .material-icons, .material-icons-outlined {
+              min-width: 1em;
+              max-width: 1em;
+              overflow: hidden;
+            }
+        </style>
+      </head>
+      <body class="h-full bg-white text-neutral-900">
+        <%= render_slot(@inner_block) %>
+      </body>
+    </html>
+    """
+  end
+
   # attr :class, :string, default: nil
   # attr :rest, :global
   # slot :inner_block, required: true
