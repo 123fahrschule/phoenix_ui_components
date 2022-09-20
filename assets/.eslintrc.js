1e59af6be9cf9a6384d56a744a47d0ba822c5531
Illia Melnykov <33714167+illiamelnykov@users.noreply.github.com>
2022-09-20 13:37:47 +0300

    Add linters and formatters

---
+++ assets/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {}
+};
