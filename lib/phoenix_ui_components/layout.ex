cb34fbfd3f0050e67ca61d0aa698acb7be3d3c1e
Illia Melnykov <33714167+illiamelnykov@users.noreply.github.com>
2023-01-05 16:27:39 +0200

    Add admin_layout component

--- lib/phoenix_ui_components/layout.ex
+++ lib/phoenix_ui_components/layout.ex
@@ -1,5 +1,7 @@
 defmodule PhoenixUiComponents.Layout do
   use PhoenixUiComponents, :component
+  import PhoenixUiComponents.SideNav
+  import PhoenixUiComponents.TopNav
 
   slot(:head, default: [])
   slot(:inner_block, required: true)
@@ -45,6 +47,30 @@ defmodule PhoenixUiComponents.Layout do
         overflow: hidden;
       }
     </style>
+    """
+  end
+
+  attr(:app_name, :string, default: nil)
+  attr(:root_path, :string, default: nil)
+  attr(:nav_sections, :list, required: true)
+
+  slot(:inner_block, required: true)
+
+  def admin_layout(assigns) do
+    ~H"""
+    <div class="grid grid-cols-[240px_auto] grid-rows-[64px_auto] h-full bg-neutral-100">
+      <PhoenixUiComponents.SideNav.side_nav
+        app_name={@app_name}
+        root_path={@root_path}
+        sections={@nav_sections}
+        class="row-span-2"
+      >
+      </PhoenixUiComponents.SideNav.side_nav>
+      <PhoenixUiComponents.TopNav.top_nav_container />
+      <main class="overflow-auto">
+        <%= render_slot(@inner_block) %>
+      </main>
+    </div>
     """
   end
 end
