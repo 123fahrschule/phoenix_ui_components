771f8474564c57173ff8eaf6ceaf8d566920c0ea
Illia Melnykov <33714167+illiamelnykov@users.noreply.github.com>
2023-01-16 10:42:45 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhoenixUiComponents.MixProject do
   def project do
     [
       app: :phoenix_ui_components,
-      version: "1.0.8",
+      version: "1.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
