111ee5ee04e8ab64bec96b9099f428ecfa096673
Danyil Mashchenko <6421808+DonCheck@users.noreply.github.com>
2023-06-14 14:38:15 +0300

    Bump phoenix live view version

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule PhoenixUiComponents.MixProject do
       # Productive. Reliable. Fast. A productive web framework that does not compromise speed and maintainability.
       {:phoenix, "~> 1.6.15 or ~> 1.7"},
       # Rich, real-time user experiences with server-rendered HTML
-      {:phoenix_live_view, "~> 0.18.3"},
+      {:phoenix_live_view, "~> 0.18"},
 
       # Dev dependencies
       {:esbuild, "~> 0.5", only: :dev}
