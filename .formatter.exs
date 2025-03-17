aed311ad790e05953b76444d63ceaf03c6c06375
Illia Melnykov <33714167+illiamelnykov@users.noreply.github.com>
2025-03-17 15:33:29 +0200

    update formatter config

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,7 @@
 # Used by "mix format"
 [
   plugins: [Phoenix.LiveView.HTMLFormatter],
+  migrate_eex_to_curly_interpolation: false,
   inputs: [
     "{mix,.formatter}.exs",
     "{config,lib,test}/**/*.{heex,ex,exs}",
