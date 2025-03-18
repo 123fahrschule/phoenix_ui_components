2fd57371a4282434604df389d017e75f8d5b33fe
Illia Melnykov <33714167+illiamelnykov@users.noreply.github.com>
2025-03-18 09:36:10 +0200

    add tailwind plugin for phoenix

---
+++ assets/tailwind_plugins/phoenix.js
@@ -0,0 +1,11 @@
+const plugin = require('tailwindcss/plugin');
+
+module.exports = plugin(function ({ addVariant }) {
+  addVariant('phx-click-loading', ['.phx-click-loading&', '.phx-click-loading &']);
+
+  addVariant('phx-submit-loading', ['.phx-submit-loading&', '.phx-submit-loading &']);
+
+  addVariant('phx-change-loading', ['.phx-change-loading&', '.phx-change-loading &']);
+
+  addVariant('phx-no-feedback', ['.phx-no-feedback&', '.phx-no-feedback &']);
+});
