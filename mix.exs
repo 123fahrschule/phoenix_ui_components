3caa93baf8e3cf0bd1c7f03e055c24cc4193c6af
Illia Melnykov <33714167+illiamelnykov@users.noreply.github.com>
2025-04-10 17:31:54 +0300

    check warnings before release

--- mix.exs
+++ mix.exs
@@ -63,6 +63,7 @@ defmodule PhoenixUiComponents.MixProject do
       dev: "run dev.exs",
       release: [
         "cmd mix format --check-formatted",
+        "cmd mix compile --force --warnings-as-errors",
         "cmd git tag #{@version}",
         "cmd git push",
         "cmd git push --tags"
