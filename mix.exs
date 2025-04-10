ccf68c7a6bbe9d79bb1348f7e0a50e690c5763d1
Illia Melnykov <33714167+illiamelnykov@users.noreply.github.com>
2025-04-10 17:27:06 +0300

    check formatting before release

--- mix.exs
+++ mix.exs
@@ -62,6 +62,7 @@ defmodule PhoenixUiComponents.MixProject do
       ],
       dev: "run dev.exs",
       release: [
+        "cmd mix format --check-formatted",
         "cmd git tag #{@version}",
         "cmd git push",
         "cmd git push --tags"
