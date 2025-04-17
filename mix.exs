87d2cdf420da47b0ee1be839ebf82cdf58576eb2
Illia Melnykov <33714167+illiamelnykov@users.noreply.github.com>
2025-04-17 10:03:49 +0300

    add support for tests

--- mix.exs
+++ mix.exs
@@ -8,8 +8,10 @@ defmodule PhoenixUiComponents.MixProject do
       app: :phoenix_ui_components,
       version: @version,
       elixir: "~> 1.13",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      test_paths: ["test", "lib"],
       aliases: aliases()
     ]
   end
@@ -20,6 +22,10 @@ defmodule PhoenixUiComponents.MixProject do
       extra_applications: []
     ]
   end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
